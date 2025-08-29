3b276f0ca372e21944a2485fe15e525915d33dfa
Satyam-Private <satyampatil1252@gmail.com>
2025-08-29 15:21:24 +0530

    Basics of java

---
+++ JavaBasics.java
@@ -0,0 +1,32 @@
+// public class JavaBasics { 
+//     public static void main(String args[]){ 
+//         System.out.println("hello"); 
+//     }
+// }
+import java.util.*;
+public class JavaBasics {
+    public static void main(String[] args) {
+        System.out.println("hello"); 
+
+        byte newVar = 9; 
+        System.out.println(newVar);
+        
+        char nameIni = 's'; 
+        System.out.println(nameIni);
+
+
+        int num = 10, num2 = 14; 
+
+        System.out.println(num + num2);
+
+        System.out.println(num * num2);
+
+        Scanner sc = new Scanner(System.in); 
+
+        int num5= sc.nextInt(); 
+
+        System.out.println(num5);
+
+    } 
+
+}
---
+++ add2num.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class add2num{ 
+    public static void main(String args[]){ 
+        System.out.println("hello this is the program for addition");
+
+
+        Scanner sc = new Scanner(System.in); 
+
+
+        int num1 = sc.nextInt(); 
+
+        int num2 = sc.nextInt(); 
+
+
+        int num3 = num1 + num2;
+
+        System.out.println("the addition is " + num3);
+    }
+}
